42a4bcad016fd5d93abf127cba73c2988cf2ecd3
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-01-31 01:29:03 +0700

    GridView fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mediaQueryData = MediaQuery.of(context);
+    final double widthScreen = mediaQueryData.size.width;
+    final double appBarHeight = kToolbarHeight;
+    final double paddingTop = mediaQueryData.padding.top;
+    final double paddingBottom = mediaQueryData.padding.bottom;
+    final double heightScreen = mediaQueryData.size.height - paddingBottom - paddingTop - appBarHeight;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,34 +23,49 @@ class MyApp extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            color: Colors.yellowAccent,
-            child: Center(
-              child: Text("1", style: TextStyle(fontSize: 24.0),),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: widthScreen/heightScreen,
+          children: <Widget>[
+            Container(
+              color: Colors.yellowAccent,
+              child: Center(
+                child: Text(
+                  "1",
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.blueAccent,
-            child: Center(
-              child: Text("2", style: TextStyle(fontSize: 24.0),),
+            Container(
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text(
+                  "2",
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.brown,
-            child: Center(
-              child: Text("3", style: TextStyle(fontSize: 24.0),),
+            Container(
+              color: Colors.brown,
+              child: Center(
+                child: Text(
+                  "3",
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.orange,
-            child: Center(
-              child: Text("4", style: TextStyle(fontSize: 24.0),),
+            Container(
+              color: Colors.orange,
+              child: Center(
+                child: Text(
+                  "4",
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
